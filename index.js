2b050fbac9eb35c14752fbb2f7ac738efa65a99a
meryldakin <meryl.k.dakin@gmail.com>
2017-04-25 15:26:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const app = "I don't do much."
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    return counter++
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+
+function createAnimal(animalType) {
+  function monsterMaker(deadlyDevice) {
+    return {
+      "animalType": animalType,
+      "deadlyDevice": deadlyDevice
+    }
+  }
+
+  return monsterMaker
+}
+
+var sharkCreator = createAnimal("Shark")
+var sharkWithFrickinLaserbeam = createAnimal("Shark")("Laserbeam")
+var sharkWithFrickinCannon = createAnimal("Shark")("Cannon")
